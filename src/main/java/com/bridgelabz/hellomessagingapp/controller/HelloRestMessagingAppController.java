b0ad10ad9b43af5b90aad6be689fb6d5fe42b466
Arun <rarnkmrbridgelabz.com>
2022-08-13 10:58:40 +0530

    Adding UC-2

--- src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestMessagingAppController.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestMessagingAppController.java
@@ -2,13 +2,19 @@ package com.bridgelabz.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestMessagingAppController {
-    @GetMapping(value = {"", "/", "/home"})
+    @GetMapping(value = {"", "/", "/home"}) 
     public String sayHello(){
         return "Hello From Bridgelabz!";
+    }
+
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name){
+        return "Hello "+name+" From Bridgelabz";
     }
 }
